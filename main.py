fd7651285b7c62cbb53ff52a9aec80d68c2214c6
Joel Grayson <joel@joelgrayson.com>
2025-02-15 12:07:15 -0800

    reads 5 seconds worth of info

--- main.py
+++ main.py
@@ -1,43 +1,34 @@
 # brainflow, numpy, matplotlib
 
 from brainflow.board_shim import BoardShim, BrainFlowInputParams, BoardIds
-from brainflow.data_filter import DataFilter, FilterTypes
-import numpy as np
-import matplotlib.pyplot as plt
+# from brainflow.data_filter import DataFilter, FilterTypes
 import time
 
-# brainflow, numpy, matplotlib
-
-from brainflow.board_shim import BoardShim
-
 # https://brainflow.readthedocs.io/en/stable/UserAPI.html#python-api-reference
-board_id=2 #CYTON_DAISY_BOARD
-
-
+board_id=BoardIds.CYTON_DAISY_BOARD.value #2 #CYTON_DAISY_BOARD
 params=BrainFlowInputParams()
-# params.serial_port=
-
-# https://brainflow.readthedocs.io/en/stable/UserAPI.html#python-api-reference
-# CYTON_DAISY_BOARD=2
-
-
 # find from ls /dev/.*
-serial_port='/dev/ttyUSB0'
-# '/dev/tty.usbserial-DP05IK99'
-
-# tty.usbserial-DP05IK99
-# params.serial_port = '/dev/cu.usbserial-DP05IK99'  # Update this line
-params.serial_port = '/dev/tty.usbserial-DP05IK99'
-
-# https://brainflow.readthedocs.io/en/stable/UserAPI.html#python-api-reference
-# CYTON_DAISY_BOARD = 2
-
-# find from ls /dev/.*
-# serial_port = '/dev/cu.usbserial-DP05IK99'  # Update this line
+# params.serial_port = '/dev/tty.usbserial-DP05IK99'
+params.serial_port = '/dev/cu.usbserial-DP05IK99'
 
 try:
+    print("PRE")
     board=BoardShim(board_id, params)
+    print("B")
     board.prepare_session()
+    print("Successfully connected")
+    board.start_stream()
+    print("Getting board data")
+    time.sleep(5)
+    data=board.get_board_data()
+    board.stop_stream()
+    board.release_session()
+    board.get_eeg_channels(board_id)
+    sampling_rate=board.get_sampling_rate(board_id)
+#     print(data)
 except Exception as e:
     print("Problem", e)
 print("Prepared")
+
+# board.release_session()
+
