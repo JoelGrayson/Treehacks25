4551c0f80affdcc6f43ab4e9e39167a58a8b9610
Joel Grayson <joel@joelgrayson.com>
2025-02-15 12:26:22 -0800

    livestreaming data

--- hello.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from attempt2!")
-
-
-if __name__ == "__main__":
-    main()
--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-# brainflow, numpy, matplotlib
-
 from brainflow.board_shim import BoardShim, BrainFlowInputParams, BoardIds
 # from brainflow.data_filter import DataFilter, FilterTypes
 import time
@@ -7,28 +5,23 @@ import time
 # https://brainflow.readthedocs.io/en/stable/UserAPI.html#python-api-reference
 board_id=BoardIds.CYTON_DAISY_BOARD.value #2 #CYTON_DAISY_BOARD
 params=BrainFlowInputParams()
-# find from ls /dev/.*
-# params.serial_port = '/dev/tty.usbserial-DP05IK99'
 params.serial_port = '/dev/cu.usbserial-DP05IK99'
 
-try:
-    print("PRE")
-    board=BoardShim(board_id, params)
-    print("B")
-    board.prepare_session()
-    print("Successfully connected")
-    board.start_stream()
-    print("Getting board data")
-    time.sleep(5)
-    data=board.get_board_data()
-    board.stop_stream()
-    board.release_session()
-    board.get_eeg_channels(board_id)
-    sampling_rate=board.get_sampling_rate(board_id)
-#     print(data)
-except Exception as e:
-    print("Problem", e)
-print("Prepared")
+board=BoardShim(board_id, params)
+board.prepare_session()
+board.start_stream()
+# time.sleep(5)
+# data=board.get_board_data()
 
-# board.release_session()
+# for i in range(1000):
+
+# About brainflow data as microvolts
+
+while True:
+    time.sleep(0.1)
+    current_data=board.get_current_board_data(25)
+    print(current_data)
+
+board.stop_stream()
+board.release_session()
 
