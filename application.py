789795bb3798732c2ecf8d95ece4f9a595a529b4
Joel Grayson <joel@joelgrayson.com>
2025-02-15 21:39:31 -0800

    try subtracting baseline

--- application.py
+++ application.py
@@ -22,55 +22,64 @@ def route(ws): #sends { type: letter | eeg, data: {} | '' }
     params=BrainFlowInputParams()
     params.serial_port='/dev/cu.usbserial-DP05IK99'
 
-    try:
-        board = BoardShim(board_id, params)
-        print("PRE")
-        board.prepare_session()
-        print("Successfully connected")
-        board.start_stream()
-        while (True):
-            time.sleep(1)
-            data = board.get_board_data()
-            eeg_channels = BoardShim.get_eeg_channels(board_id)
-            accel_channels = BoardShim.get_accel_channels(board_id)
-            sampling_rate=board.get_sampling_rate(board_id)
+    board = BoardShim(board_id, params)
+    print("PRE")
+    board.prepare_session()
+    print("Successfully connected")
+    board.start_stream()
 
-            arr_left = data[eeg_channels[1]]
-            
-            #mid = arr[int(0.25 * len(arr)):int(0.75 * len(arr))]
-            #range_mid = np.ptp(mid)
-            range_left = np.ptp(arr_left)
+    for i in range(3):
+        time.sleep(1)
+        data=board.get_board_data()
+    time.sleep(1)
+    baseline=board.get_board_data()
+    print("Set baseline to", baseline, type(baseline))
+    
+    eeg_channels = BoardShim.get_eeg_channels(board_id)
+    accel_channels = BoardShim.get_accel_channels(board_id)
+    sampling_rate=board.get_sampling_rate(board_id)
 
-            arr_right = data[eeg_channels[9]]
-            range_right = np.ptp(arr_right)
-            
-            if range_left < 200 and range_right < 200:
-                print("baseline")
-            elif range_left > range_right:
-                print("0")
-            elif range_right > range_left:
-                print("1")
+    while (True):
+        time.sleep(1)
+        data = board.get_board_data()
 
-            channelToData={}
-            for eeg_channel in eeg_channels:
-                channelToData[eeg_channel]=list(data[eeg_channel])
-            ws.send(json.dumps({ 'type': 'eeg', 'data': channelToData }))
-
-            '''
-            accel = data[accel_channels[2]]
-            range_accel = np.ptp(accel)
-            if (range > 200):
-                print("0")
-            elif (range_accel > 0.14):
-                print("1")
-            else:
-                print("baseline")'''
+        arr_left = data[eeg_channels[1]]
         
-        board.stop_stream()
-        board.release_session()
-    except Exception as e:
-        print("Problem", e)
-    print("Prepared")
+        #mid = arr[int(0.25 * len(arr)):int(0.75 * len(arr))]
+        #range_mid = np.ptp(mid)
+        range_left = np.ptp(arr_left)
+
+        arr_right = data[eeg_channels[9]]
+        range_right = np.ptp(arr_right)
+        
+        if range_left < 200 and range_right < 200:
+            print("baseline")
+        elif range_left > range_right:
+            print("0")
+        elif range_right > range_left:
+            print("1")
+
+        channelToData={}
+        for eeg_channel in eeg_channels:
+            print(len(data[eeg_channel]), len(baseline[eeg_channel]))
+            if len(data[eeg_channel]) == len(baseline[eeg_channel]):
+                channelToData[eeg_channel]=list(data[eeg_channel]-baseline[eeg_channel])
+            else:
+                print(f"Skipping channel {eeg_channel} due to shape mismatch")
+        ws.send(json.dumps({ 'type': 'eeg', 'data': channelToData }))
+
+        '''
+        accel = data[accel_channels[2]]
+        range_accel = np.ptp(accel)
+        if (range > 200):
+            print("0")
+        elif (range_accel > 0.14):
+            print("1")
+        else:
+            print("baseline")'''
+    
+    board.stop_stream()
+    board.release_session()
 
     '''SAMPLING_RATE=125 #print("Sampling rate", BoardShim.get_sampling_rate(board_id))
 
