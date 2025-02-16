93692d9b1737b30b33d03bc3d948e1b60ebc8e61
Joe Li <joe5050.li@gmail.com>
2025-02-15 18:29:06 -0800

    force

--- main.py
+++ main.py
@@ -3,14 +3,75 @@ from brainflow.data_filter import DataFilter, FilterTypes
 import time
 import numpy as np
 
+
+from brainflow.board_shim import BoardShim, BoardIds
+
+
 # https://brainflow.readthedocs.io/en/stable/UserAPI.html#python-api-reference
 board_id=BoardIds.CYTON_DAISY_BOARD.value #2 #CYTON_DAISY_BOARD
 params=BrainFlowInputParams()
 params.serial_port='/dev/cu.usbserial-DP05IK99'
 
-board=BoardShim(board_id, params)
-board.prepare_session()
-board.start_stream()
+
+
+try:
+    print("PRE")
+    print("B", board_id)
+    board = BoardShim(board_id, params)
+    board.prepare_session()
+    print("Successfully connected")
+    board.start_stream()
+    print("Getting board data")
+    time.sleep(3)
+    data=board.get_board_data()
+    while (True):
+        time.sleep(1)
+        data = board.get_board_data()
+        eeg_channels = BoardShim.get_eeg_channels(board_id)
+        accel_channels = BoardShim.get_accel_channels(board_id)
+        sampling_rate=board.get_sampling_rate(board_id)
+
+        arr_left = data[eeg_channels[1]]
+        
+        #mid = arr[int(0.25 * len(arr)):int(0.75 * len(arr))]
+        #range_mid = np.ptp(mid)
+        range_left = np.ptp(arr_left)
+
+        arr_right = data[eeg_channels[9]]
+        range_right = np.ptp(arr_right)
+        
+        if range_left < 200 and range_right < 200:
+            print("baseline")
+        elif range_left > range_right:
+            print("0")
+        elif range_right > range_left:
+            print("1")
+        '''
+        accel = data[accel_channels[2]]
+        range_accel = np.ptp(accel)
+        if (range > 200):
+            print("0")
+        elif (range_accel > 0.14):
+            print("1")
+        else:
+            print("baseline")'''
+
+
+
+        '''arr = []
+
+        for channel in eeg_channels:
+            print(channel)
+            arr.append(data[channel])'''
+       # print(f"Channel {channel}: {np.mean(data[channel])}")
+    #for channel in accel_channels:
+    #    arr.append(data[channel])
+    np.savetxt('data.csv', arr, delimiter=",")
+    board.stop_stream()
+    board.release_session()
+except Exception as e:
+    print("Problem", e)
+print("Prepared")
 
 SAMPLING_RATE=125 #print("Sampling rate", BoardShim.get_sampling_rate(board_id))
 
